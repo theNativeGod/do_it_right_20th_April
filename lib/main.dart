8f93cbeaf162892f22813e3e62d01f7611010a4d
Saswata Saha <saswatasaha@Saswatas-MacBook-Air.local>
2024-04-20 20:49:24 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:do_it_right/views/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: 'Poppins',
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
